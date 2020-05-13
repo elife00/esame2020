5ac8a2d391a929ba96413904fe1e8fb06a28fb2a
Elisabetta <eli.ferri.2000@gmail.com>
2020-05-13 20:10:18 +0200

    files have been added to the project. Let's start!

---
+++ epidemic.cpp
@@ -0,0 +1,276 @@
+#include <algorithm>
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <string>
+#include <thread>
+#include <vector>
+
+enum class State : char { Susceptible, Infected, Recovered };
+
+struct Situation
+{
+  int t = 0;
+  int s = 0;
+  int i = 0;
+  int r = 0;
+};
+
+///
+
+class Population
+{
+  int n_;
+  std::vector<State> population_;
+  std::vector<unsigned int> stay_;
+  std::vector<Situation> evolution_;
+
+ public:
+  Population(int const& n)
+      : n_{n}
+      , population_(n * n, State::Susceptible)
+      , stay_(n_ * n_, 0)
+      , evolution_{{0, n * n, 0, 0}}
+  {
+  }
+
+  std::vector<State>& vector()
+  {
+    return population_;
+  }
+
+  State& set(int& x, int& y, State s)
+  {
+    assert(x > 0 && x <= n_ && y > 0 && y <= n_);
+    population_[(x - 1) + (y - 1) * n_] = s;
+  }
+
+  State get(int const& x, int const& y) const
+  {
+    return (x < 0 || x >= n_ || y < 0 || y >= n_)
+               ? State::Susceptible
+               : population_[(x - 1) + (y - 1) * n_];
+  }
+
+  auto infection(double const& ratInf)
+  {
+    for (int i = 0; i != n_ * n_ * ratInf; ++i) {
+      population_[i] = State::Infected;
+    }
+    std::random_device sed;
+    std::mt19937 gen(sed());
+    std::shuffle(population_.begin(), population_.end(), gen);
+  }
+
+  int contact(int const& x, int const& y) const
+  {
+    assert(x > 0 && x <= n_ && y > 0 && y <= n_);
+
+    int infected = 0;
+
+    for (int i = x - 1; i != x + 2; ++i) {
+      for (int j = y - 1; j != y + 2; ++j) {
+        if (x < 0 || x >= n_ || y < 0 || y >= n_) {
+        } else if (population_[(x - 1) + (y - 1) * n_] == State::Infected) {
+          ++infected;
+        }
+      }
+    }
+    assert(infected < 9 && infected > -1);
+    return infected;
+  }
+
+  Population epidemic(double const& pInf, double const& pGua) const
+  {
+    assert(pInf > 0 && pInf < 1 && pGua > 0 && pGua < 1);
+    Population next(n_);
+    next.evolution_ = evolution_;
+    next.stay_ = stay_;
+    Situation sit = {0, 0, 0, 0};
+
+    for (int x = 1; x != n_ + 1; ++x) {
+      for (int y = 1; y != n_ + 1; ++y) {
+        int coordinate = (x - 1) + (y - 1) * n_;
+        if (population_[coordinate] == State::Susceptible) {
+          ++sit.s;
+          int infected = contact(x, y);
+          int infections = 0;
+          for (int i = 0; i != infected; ++i) {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<> dis(0.0, 1.0);
+
+            double probability = dis(gen);
+            if (probability <= pInf) {
+              ++infections;
+            }
+          }
+          if (infections > 0) {
+            next.set(x, y, State::Infected);
+          } else {
+            next.set(x, y, State::Susceptible);
+          }
+        }
+
+        if (population_[coordinate] == State::Infected) {
+          ++sit.i;
+          ++next.stay_[coordinate];
+
+          std::random_device rd;
+          std::mt19937 gen(rd());
+          std::uniform_real_distribution<> dis(0., 1.0);
+          if (dis(gen) <= pGua) {
+            next.set(x, y, State::Recovered);
+
+          } else {
+            next.set(x, y, State::Infected);
+          }
+        }
+
+        if (population_[coordinate] == State::Recovered) {
+          ++sit.r;
+          next.set(x, y, State::Recovered);
+        }
+      }
+    }
+
+    sit.t = evolution_.back().t + 1;
+    next.evolution_.push_back({sit});
+    return next;
+  }
+
+  void draw() const
+  {
+    std::string result;
+    int i = 0;
+    for (auto const& v : population_) {
+      std::string symbol;
+      switch (v) {
+        case State::Susceptible:
+          symbol = '|';
+          break;
+        case State::Infected:
+          symbol = "\033[1;31m|\033[0m";
+          break;
+        case State::Recovered:
+          symbol = ' ';
+          break;
+      }
+      if (i % n_ == 0) {
+        result += '\n';
+        result += symbol;
+        result += ' ';
+      } else {
+        result += symbol;
+        result += ' ';
+      }
+      ++i;
+    }
+    std::cout << result << '\n';
+  }
+
+  void draw_stay() const
+  {
+    for (int y = n_; y > 0; --y) {
+      for (int x = 1; x <= n_; ++x) {
+        std::cout << stay_[(y - 1) + (x - 1) * n_];
+      }
+      std::cout << '\n';
+    }
+  }
+
+  void trend() const
+  {
+    /*std::cout << std::setw(4) << "days" << ' ' << std::setw(4) << "S" << ' '
+              << std::setw(4) << "I" << ' ' << std::setw(4) << "R" << '\n';*/
+    for (auto& v : evolution_) {
+      std::cout << std::setw(4) << v.t << ' ' << std::setw(4) << v.s << ' '
+                << std::setw(4) << v.i << ' ' << std::setw(4) << v.r << '\n';
+    }
+  }
+
+  bool count() const
+  {
+    int I = 0;
+    int S = 0;
+    int R = 0;
+    for (auto const& v : population_) {
+      if (v == State::Susceptible) {
+        ++S;
+      } else if (v == State::Infected) {
+        ++I;
+      } else if (v == State::Recovered) {
+        ++R;
+      }
+    }
+    if (I == 0) {
+      return true;
+    }
+
+    std::cout << "S = " << S << '\n';
+    std::cout << "I = " << I << '\n';
+    std::cout << "R = " << R << '\n';
+    std::cout << "N = " << S + R + I << '\n';
+
+    return false;
+  }
+
+  void parameters()
+  {
+    std::vector<double> beta;
+    std::vector<double> gamma;
+    for (int i = 0; i != evolution_.size() - 1;
+         ++i) {  // le formule sono con le percentuali
+      double Savg = 0.5 * (evolution_[i + 1].s + evolution_[i].s) / n_ * n_;
+      double Iavg = 0.5 * (evolution_[i + 1].i + evolution_[i].i) / n_ * n_;
+      double Ravg = 0.5 * (evolution_[i + 1].r + evolution_[i].r) / n_ * n_;
+      double dS = (evolution_[i + 1].s - evolution_[i].s) / n_ * n_;
+      double dI = (evolution_[i + 1].i - evolution_[i].i) / n_ * n_;
+      double dR = (evolution_[i + 1].r - evolution_[i].r) / n_ * n_;
+      double b = (dR + dI) / (Savg * Iavg);
+      double g = dR / Iavg;
+      beta.push_back(b);
+      gamma.push_back(g);
+    }
+    for (int i = 0; i != beta.size(); ++i) {
+      std::cout << beta[i] << ' ' << gamma[i] << '\n';
+    }
+  }
+};
+
+int main()
+{
+  int dim = 10;
+  double pInf = 0.15;
+  double pGua = 0.1;  // 1 giorno = 100 milliseconds
+  double ratInf = 0.2;
+
+  Population population(dim);
+  std::cout << "How an epidemic begin:\n";
+
+  population.infection(ratInf);
+  population.draw();
+  population.count();
+
+  std::cout << '\n' << "Epidemic has began\n";
+
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+
+  int days = 0;
+  while (true) {
+    ++days;
+    std::cout << "\033c";
+    population = population.epidemic(pInf, pGua);
+    population.draw();
+
+    if (population.count()) {
+      break;
+    }
+    std::cout << "giorni dall'inizio dell'epidemia: " << days << '\n';
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+  population.trend();
+  std::cout << '\n' << '\n';
+  population.parameters();
+}
---
+++ gui.cpp
@@ -0,0 +1,165 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <thread>
+
+
+
+class TileMap : public sf::Drawable, public sf::Transformable // classe figlia
+{
+private:
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // apply the transform
+        states.transform *= getTransform();
+
+        // apply the tileset texture
+        states.texture = &m_texture;
+
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_texture;
+
+public:
+
+    bool load(const std::string& texture, sf::Vector2u tileSize, std::vector<int> tiles, unsigned int width, unsigned int height)
+    {                                   // dimensione della piastrella                              diensione della griglia
+        // load the tileset texture
+        if (!m_texture.loadFromFile(texture))
+            return false;
+
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::Quads); // riorganizza i vertici in quadrati
+        m_vertices.resize(width * height * 4);
+
+        // populate the vertex array, with one quad per tile
+        for (unsigned int i = 0; i < width; ++i)
+            for (unsigned int j = 0; j < height; ++j)
+            {
+                // get the current tile number
+                int tileNumber = tiles[i + j * width];  // questo diventerebbe stato // 1
+
+                // find its position in the tileset texture
+                int tu = tileNumber % (m_texture.getSize().x / tileSize.x); // 5
+                int tv = tileNumber / (m_texture.getSize().x / tileSize.x); // 3/5
+
+                // get a pointer to the current tile's quad
+                sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+
+                // define its 4 corners
+                quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+                quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+                quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+
+                // define its 4 texture coordinates
+                quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+                quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+                quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+            }
+
+        return true;
+    }
+
+
+};
+
+int main()
+{
+    sf::RenderWindow epidemic(sf::VideoMode(800, 600), "My epidemic");
+    sf::RenderWindow graph(sf::VideoMode(600, 600), "My graph");
+    graph.setVerticalSyncEnabled(true); // call it once, after creating the window
+
+
+    // change the position of the window (relatively to the desktop)
+    epidemic.setPosition(sf::Vector2i(10, 50));
+    graph.setPosition(sf::Vector2i(850, 50));
+
+        sf::Texture texture;
+        sf::Image image;
+        if (!texture.loadFromFile("image.png"))
+        {
+            return EXIT_FAILURE;;
+        }
+        texture.setSmooth(true);
+        sf::Sprite sprite;
+        sprite.setTexture(texture);
+        auto n = texture.getSize();
+        float x = (600-n.x) / 2;
+        float y = (600-n.y) / 2;
+        sprite.setPosition(sf::Vector2f(x, y)); // absolute position
+
+        std::vector<int> level =
+    {
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    };
+
+        TileMap map;
+
+    // run the program as long as the window is open
+    while (epidemic.isOpen() || graph.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (epidemic.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed) {
+                epidemic.close();
+                graph.close();
+            }
+        }
+        while (graph.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed) {
+                graph.close();
+            }
+        }
+
+        // clear the window with black color
+        graph.clear(sf::Color::Black);
+
+        // draw everything here...
+        graph.draw(sprite);
+
+        // end the current frame
+        graph.display();
+        /*Calling display is also mandatory,
+        it takes what was drawn since the last
+        call to display and displays it on 
+        the window. Indeed, things are not drawn
+        directly to the window, but to a hidden buffer.
+        This buffer is then copied to the window when
+        you call display*/
+
+
+        if (!map.load("tileset.png", sf::Vector2u(32, 32), level, 16, 8))
+            return -1;
+
+        // draw the map
+        epidemic.clear();
+        epidemic.draw(map);
+        epidemic.display();
+        for (int i =0; i != level.size(); ++i) {
+            ++level[i];
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+
+    }
+
+    return 0;
+}
