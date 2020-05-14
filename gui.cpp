b5bbdb5f3020616fbdaa516a3891e4bcd9516233
Elisabetta <eli.ferri.2000@gmail.com>
2020-05-14 21:29:27 +0200

    esperimento testo su gui

--- gui.cpp
+++ gui.cpp
@@ -71,6 +71,7 @@ public:
 
 };
 
+
 int main()
 {
     sf::RenderWindow epidemic(sf::VideoMode(800, 600), "My epidemic");
@@ -112,6 +113,7 @@ int main()
 
         TileMap map;
 
+        sf::Text text = L"😀";
     // run the program as long as the window is open
     while (epidemic.isOpen() || graph.isOpen())
     {
@@ -138,6 +140,7 @@ int main()
 
         // draw everything here...
         graph.draw(sprite);
+        graph.draw(text);
 
         // end the current frame
         graph.display();
@@ -166,4 +169,37 @@ int main()
     }
 
     return 0;
+
+    /*  int dim = 10;
+  double pInf = 0.15;
+  double pGua = 0.1;  // 1 giorno = 100 milliseconds
+  double ratInf = 0.2;
+
+  Population population(dim);
+  std::cout << "How an epidemic begin:\n";
+
+  population.infection(ratInf);
+  population.draw();
+  population.count();
+
+  std::cout << '\n' << "Epidemic has began\n";
+
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+
+  int days = 0;
+  while (true) {
+    ++days;
+    std::cout << "\033c";
+    population = population.epidemic(pInf, pGua);
+    population.draw();
+
+    if (population.count()) {
+      break;
+    }
+    std::cout << "giorni dall'inizio dell'epidemia: " << days << '\n';
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+  population.trend();
+  std::cout << '\n' << '\n';
+  population.parameters();*/
 }
