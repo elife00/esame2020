1796a365b0735c8edf45bd0e6142cc569d0955a2
Elisabetta Ferri <65276944+Ferri-00@users.noreply.github.com>
2020-05-20 12:59:34 +0200

    corected x and y in contact with i and j

--- epidemic.hpp
+++ epidemic.hpp
@@ -76,7 +76,7 @@ class Population
     for (int i = x - 1; i != x + 2; ++i) {
       for (int j = y - 1; j != y + 2; ++j) {
         if (x < 0 || x >= n_ || y < 0 || y >= n_) {
-        } else if (population_[(x - 1) + (y - 1) * n_] == I) {
+        } else if (population_[(i - 1) + (j - 1) * n_] == I) {
           ++infected;
         }
       }
